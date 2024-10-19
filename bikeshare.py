34f5048681bac391ad98b3c2e4789ef4dc749f9d
unknown <namin.sima@gmail.com>
2024-10-19 14:03:43 -0400

    refactor: edited a comment.

--- bikeshare.py
+++ bikeshare.py
@@ -119,7 +119,7 @@ def station_stats(df):
 
 
 def trip_duration_stats(df):
-    """Display statistics on the total and average trip duration."""
+    """Displays statistics on the total and average trip duration."""
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
 
